245ad8665a3c3cc057f6614b6170dfdfa0c643ad
wsmyc <noureddine.yahiacherif.info@gmail.com>
2026-06-06 18:51:04 +0100

    built the attention method and the MHA Block also created the residual connections of the encoder class and instance

--- model.py
+++ model.py
@@ -66,6 +66,73 @@ class FeedForwardBlock(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model:int, num_heads:int, dropout:float):
         super().__init__()
-        self.d_model = d_model
+        self.d_model = d_model #should be divisible by the number of heads
         self.num_heads = num_heads
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        self.d_k = d_model // num_heads #dimension of each head
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
         self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1)) / maths.sqrt(d_k) #calculate the attention scores
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9) #mask the attention scores where the mask is 0
+        attention_scores = attention_scores.softmax(dim=-1) #apply softmax to get the attention weights
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores #return the output of the attention and the attention scores for visualization also here we are multiplying the attention weights with the value to get the output of the attention
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) #(Batch, Seq_Len, d_model) -> (Batch, Seq_Len, d_model)
+        key = self.w_k(k) #(Batch, Seq_Len, d_model) -> (Batch, Seq_Len, d_model)
+        value = self.w_v(v) #(Batch, Seq_Len, d_model) -> (Batch, Seq_Len, d_model)
+
+        #(Batch, Seq_Len, d_model) -> (Batch, Seq_Len, num_heads, d_k) -> (Batch, num_heads, Seq_Len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.num_heads, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.num_heads, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.num_heads, self.d_k).transpose(1,2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.num_heads * self.d_k) #(Batch, num_heads, Seq_Len, d_k) -> (Batch, Seq_Len, d_model)
+
+        #batch, seq_len, d_model -> batch, seq_len, d_model
+        return self.w_o(x) 
+    
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNorm()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x))) #apply layer normalization to the input, then apply the sublayer, then apply dropout, and finally add the input to the output of the sublayer
+    
+class EncoderLayer(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttention, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)]) #two residual connections one for the self attention block and one for the feed forward block
+
+    def forward(self,x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,src_mask))
+        #basically we take the input x and call the forward method of multihead attention then we pass the output of the multihead attention to the first residual connection and add it to the input x
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+    class Encoder(nn.Module):
+        def __init__(self, layers : nn.ModuleList):
+            super().__init__()
+            self.layers = layers
+            self.norm = LayerNorm()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
